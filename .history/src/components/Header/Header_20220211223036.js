3f90861c090590b76e5e583e48ef3a75e3f164c8
Arllan <60104295+MrSyntax31@users.noreply.github.com>
2022-02-11 22:45:54 +0800

    Create my Portfolio

---
+++ .history/src/components/Header/Header_20220211223036.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () =>  (
+  <Container>
+    <Div1>
+      
+    </Div1>
+  </Container>
+);
+
+export default Header;
